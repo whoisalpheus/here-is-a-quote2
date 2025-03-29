14e87d2c2704c9a0c07cb7a4eddce886b4f2e32d
whoisalpheus <104221606+whoisalpheus@users.noreply.github.com>
2025-03-29 11:20:45 -0400

    Uncomment dotenv config?

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 // Pool setup
 const Pool = require('pg').Pool
 const path = require('path')
-// require('dotenv').config({ path: path.join(__dirname, '..', '.env')})
+require('dotenv').config({ path: path.join(__dirname, '..', '.env')})
 
 const pool = new Pool({
     user: process.env.PG_USER,
