06d7bcbaf02ae39c91186d7402f05a5543c607ae
whoisalpheus <104221606+whoisalpheus@users.noreply.github.com>
2025-04-08 12:37:22 -0400

    Styling fixing for shimmer effects

--- client/src/Buttons.jsx
+++ client/src/Buttons.jsx
@@ -9,20 +9,20 @@ function Buttons({ onClick }) { // onClose
 
     return (
         <Container className="bg-secondary rounded-4">
-            <h1>How are you feeling?</h1>
+            <h1>Pick a topic</h1>
             <Col>           
-                <Button id="sad" className="m-3 shadow text-black" onClick={ onClick } type="submit">Sad</Button>
-                <Button id="anger" className="m-3 shadow text-black" onClick={ onClick } type="submit">Anger</Button>
-                <Button id="anxiety" className="m-3 shadow text-black" onClick={ onClick } type="submit">Anxiety</Button>
+                <Button id="sad" className="m-3 shadow text-danger" onClick={ onClick } type="submit">Sadness</Button>
+                <Button id="anger" className="m-3 shadow text-white" onClick={ onClick } type="submit">Anger</Button>
+                <Button id="anxiety" className="m-3 shadow text-warning" onClick={ onClick } type="submit">Anxiety</Button>
             </Col>
             <Col>
-                <Button id="happiness" className="m-3 shadow text-black" onClick={ onClick } type="submit">Happiness</Button>
-                <Button id="shame" className="m-3 shadow text-black" onClick={ onClick } type="submit">Shame</Button>
-                <Button id="guilt" className="m-3 shadow text-black" onClick={ onClick } type="submit">Guilt</Button>
+                <Button id="happiness" className="m-3 shadow text-info" onClick={ onClick } type="submit">Happiness</Button>
+                <Button id="shame" className="m-3 shadow text-success" onClick={ onClick } type="submit">Shame</Button>
+                <Button id="guilt" className="m-3 shadow text-info" onClick={ onClick } type="submit">Guilt</Button>
             </Col>
             <Col>
-                <Button id="self_worth" className="m-3 shadow text-black" onClick={ onClick } type="submit">Self-Worth</Button>
-                <Button id="resilience" className="m-3 shadow text-black" onClick={ onClick } type="submit">Resilience</Button>
+                <Button id="self_worth" className="m-3 shadow text-danger" onClick={ onClick } type="submit">Self-Worth</Button>
+                <Button id="resilience" className="m-3 shadow text-white" onClick={ onClick } type="submit">Resilience</Button>
                 {/* <Button id="depression" className="m-3 shadow text-black" onClick={ onClick } type="submit">Depression</Button> */}
             </Col>
         </Container>
