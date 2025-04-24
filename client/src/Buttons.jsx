30a1998debe524547574b7143412c6990b319422
whoisalpheus <104221606+whoisalpheus@users.noreply.github.com>
2025-04-24 10:18:34 -0400

    Changed buttons to smaller size bc of mobile look...

--- client/src/Buttons.jsx
+++ client/src/Buttons.jsx
@@ -11,19 +11,19 @@ function Buttons({ onClick }) { // onClose
         <Container style={{ width: '80vw', height: '80vh' }} className="bg-transparent rounded-4">
             <h1>Pick a topic</h1>
             <Col>           
-                <Button size="lg" id="sad" className="m-5 shadow text-danger" onClick={ onClick } type="submit">Sadness</Button>
-                <Button size="lg" id="anger" className="m-5 shadow text-white" onClick={ onClick } type="submit">Anger</Button>
-                <Button size="lg" id="anxiety" className="m-5 shadow text-warning" onClick={ onClick } type="submit">Anxiety</Button>
+                <Button id="sad" className="m-5 shadow text-danger" onClick={ onClick } type="submit">Sadness</Button>
+                <Button id="anger" className="m-5 shadow text-white" onClick={ onClick } type="submit">Anger</Button>
+                <Button id="anxiety" className="m-5 shadow text-warning" onClick={ onClick } type="submit">Anxiety</Button>
             </Col>
             <Col>
-                <Button size="lg" id="happiness" className="m-5 shadow text-info" onClick={ onClick } type="submit">Happiness</Button>
-                <Button size="lg" id="shame" className="m-5 shadow text-success" onClick={ onClick } type="submit">Shame</Button>
-                <Button size="lg" id="guilt" className="m-5 shadow text-info" onClick={ onClick } type="submit">Guilt</Button>
+                <Button id="happiness" className="m-5 shadow text-info" onClick={ onClick } type="submit">Happiness</Button>
+                <Button id="shame" className="m-5 shadow text-success" onClick={ onClick } type="submit">Shame</Button>
+                <Button id="guilt" className="m-5 shadow text-info" onClick={ onClick } type="submit">Guilt</Button>
             </Col>
             <Col>
-                <Button size="lg" id="self_worth" className="m-5 shadow text-danger" onClick={ onClick } type="submit">Self-Worth</Button>
-                <Button size="lg" id="resilience" className="m-5 shadow text-white" onClick={ onClick } type="submit">Resilience</Button>
-                {/* <Button size="lg" id="depression" className="m-5 shadow text-black" onClick={ onClick } type="submit">Depression</Button> */}
+                <Button id="self_worth" className="m-5 shadow text-danger" onClick={ onClick } type="submit">Self-Worth</Button>
+                <Button id="resilience" className="m-5 shadow text-white" onClick={ onClick } type="submit">Resilience</Button>
+                {/* <Button id="depression" className="m-5 shadow text-black" onClick={ onClick } type="submit">Depression</Button> */}
             </Col>
         </Container>
 )}
